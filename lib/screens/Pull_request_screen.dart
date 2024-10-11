a059bb507e497060e510d8c39a0377f87679546f
Dikshant <dikshantahalawat2005@gmail.com>
2024-10-11 19:21:49 +0530

    homescreen

---
+++ lib/screens/Pull_request_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PullRequestScreen extends StatefulWidget {
+  const PullRequestScreen({super.key});
+
+  @override
+  State<PullRequestScreen> createState() => _PullRequestScreenState();
+}
+
+class _PullRequestScreenState extends State<PullRequestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/Repo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Repo extends StatefulWidget {
+  const Repo({super.key});
+
+  @override
+  State<Repo> createState() => _RepoState();
+}
+
+class _RepoState extends State<Repo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
